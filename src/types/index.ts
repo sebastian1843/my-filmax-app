cfd7d6845c9610cd79380d2e4cb29f2544893926
sebastian1843 <srestrepomuentes@gmail.com>
2025-05-04 10:52:18 -0500

    Agrega index.ts con interfaz MovieInterface

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface MovieInterface {
+    id: string;
+    title: string;
+    genre: string;
+    duration: string;
+    thumbnailUrl: string;
+    videoUrl: string;
+    description: string;
+  }
+  
